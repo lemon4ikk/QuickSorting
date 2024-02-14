a2ef7d5984fa1c8fbc268c1fc27523a18f1acb7d
Nikita <nikit.sambo@gmail.com>
2024-02-14 21:47:01 +0300

    Adding csv files and fixed recursion

--- QuikSort.cpp
+++ QuikSort.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 void sort(int Arr[], int begin, int end)
 {
+    if (begin > end) return;
+
     const int size = 10;
     int pivot;
     int i = begin;
@@ -24,24 +26,16 @@ void sort(int Arr[], int begin, int end)
         
         if (i <= j) 
         {
-            if (Arr[i] > Arr[j]) 
-            {
             buffer = Arr[j];
             Arr[j] = Arr[i];
             Arr[i] = buffer;
 
             i++;
             j--;
-            }
         }
     }
-
-    if(j > begin) {
-        return sort(Arr, begin, j);
-    }
-    if(end < i) {
-        return sort(Arr, i, end);
-    } 
+        sort(Arr, begin, j);
+        sort(Arr, i, end);
 }
 
 int main() {
@@ -49,13 +43,15 @@ int main() {
 
     srand(time(NULL));
 
-    int size = 10;
+    const int size = 10;
     int Arr[size];
     
-    char path[] = "QuikSort.txt";
+    char path[] = "sorted.csv";
+    char path2[] = "unsorted.csv";
 
-    fstream file;                                                       
-        file.open(path, fstream::trunc | fstream::in | fstream::out);                                   
+    fstream file, file2;                                                       
+        file.open(path, fstream::trunc | fstream::in | fstream::out);
+        file2.open(path2, fstream::trunc | fstream::in | fstream::out);                                     
 
         if (!file.is_open())
         {
@@ -68,16 +64,16 @@ int main() {
             for(int i = 0; i < size; i++)
             {
                 Arr[i] = rand() % 50;
-                cout << Arr[i] << " ";
+                file2 << Arr[i] << "\n";
             } 
 
             sort(Arr, 0, size - 1);
             
-            cout << "\n";
+            file << "\n";
 
             for (int i = 0; i < size; i++)
             {
-            cout << Arr[i] << " ";
+            file << Arr[i] << "\n";
             }
         }
 
